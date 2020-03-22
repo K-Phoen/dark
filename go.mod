9911f0344bfb5af9dc196bf6c9cfc62ad8de28f4
Kévin Gomez <kevin.gomez@voiapp.io>
2020-03-22 20:28:15 +0100

    Update grabana

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/K-Phoen/dark
 go 1.13
 
 require (
-	github.com/K-Phoen/grabana v0.4.1
+	github.com/K-Phoen/grabana v0.5.0
 	gopkg.in/yaml.v2 v2.2.8
 	k8s.io/api v0.0.0-20200319202348-eb909d5fe0e7
 	k8s.io/apimachinery v0.0.0-20200319202151-147abd67b880
@@ -11,5 +11,3 @@ require (
 	k8s.io/code-generator v0.0.0-20200319201949-6bb2b634cece
 	k8s.io/klog v1.0.0
 )
-
-replace github.com/K-Phoen/grabana => ../grabana
---
+++ vendor/github.com/K-Phoen/grabana/dashboard/dashboard.go
@@ -0,0 +1,191 @@
+package dashboard
+
+import (
+	"encoding/json"
+
+	"github.com/K-Phoen/grabana/row"
+	"github.com/K-Phoen/grabana/variable/constant"
+	"github.com/K-Phoen/grabana/variable/custom"
+	"github.com/K-Phoen/grabana/variable/interval"
+	"github.com/K-Phoen/grabana/variable/query"
+	"github.com/grafana-tools/sdk"
+)
+
+// TagAnnotation describes an annotation represented as a Tag.
+// See https://grafana.com/docs/grafana/latest/reference/annotations/#query-by-tag
+type TagAnnotation struct {
+	Name       string
+	Datasource string
+	IconColor  string `yaml:"color"`
+	Tags       []string
+}
+
+// Option represents an option that can be used to configure a
+// dashboard.
+type Option func(dashboard *Builder)
+
+// Builder is the main builder used to configure dashboards.
+type Builder struct {
+	board *sdk.Board
+}
+
+// New creates a new dashboard builder.
+func New(title string, options ...Option) Builder {
+	board := sdk.NewBoard(title)
+	board.ID = 0
+	board.Timezone = ""
+
+	builder := &Builder{board: board}
+
+	for _, opt := range append(defaults(), options...) {
+		opt(builder)
+	}
+
+	return *builder
+}
+
+func defaults() []Option {
+	return []Option{
+		defaultTimePicker(),
+		defaultTime(),
+		SharedCrossHair(),
+	}
+}
+
+func defaultTime() Option {
+	return func(builder *Builder) {
+		builder.board.Time = sdk.Time{
+			From: "now-3h",
+			To:   "now",
+		}
+	}
+}
+
+func defaultTimePicker() Option {
+	return func(builder *Builder) {
+		builder.board.Timepicker = sdk.Timepicker{
+			RefreshIntervals: []string{"5s", "10s", "30s", "1m", "5m", "15m", "30m", "1h", "2h", "1d"},
+			TimeOptions:      []string{"5m", "15m", "1h", "6h", "12h", "24h", "2d", "7d", "30d"},
+		}
+	}
+}
+
+// MarshalJSON implements the encoding/json.Marshaler interface.
+//
+// This method can be used to render the dashboard into a JSON file
+// which your configuration management tool of choice can then feed into
+// Grafana's dashboard via its provisioning support.
+// See https://grafana.com/docs/grafana/latest/administration/provisioning/#dashboards
+func (builder *Builder) MarshalJSON() ([]byte, error) {
+	return json.Marshal(builder.board)
+}
+
+// Internal.
+func (builder *Builder) Internal() *sdk.Board {
+	return builder.board
+}
+
+// VariableAsConst adds a templated variable, defined as a set of constant
+// values.
+// See https://grafana.com/docs/grafana/latest/reference/templating/#variable-types
+func VariableAsConst(name string, options ...constant.Option) Option {
+	return func(builder *Builder) {
+		templatedVar := constant.New(name, options...)
+
+		builder.board.Templating.List = append(builder.board.Templating.List, templatedVar.Builder)
+	}
+}
+
+// VariableAsCustom adds a templated variable, defined as a set of custom
+// values.
+// See https://grafana.com/docs/grafana/latest/reference/templating/#variable-types
+func VariableAsCustom(name string, options ...custom.Option) Option {
+	return func(builder *Builder) {
+		templatedVar := custom.New(name, options...)
+
+		builder.board.Templating.List = append(builder.board.Templating.List, templatedVar.Builder)
+	}
+}
+
+// VariableAsInterval adds a templated variable, defined as an interval.
+// See https://grafana.com/docs/grafana/latest/reference/templating/#variable-types
+func VariableAsInterval(name string, options ...interval.Option) Option {
+	return func(builder *Builder) {
+		templatedVar := interval.New(name, options...)
+
+		builder.board.Templating.List = append(builder.board.Templating.List, templatedVar.Builder)
+	}
+}
+
+// VariableAsQuery adds a templated variable, defined as a query.
+// See https://grafana.com/docs/grafana/latest/reference/templating/#variable-types
+func VariableAsQuery(name string, options ...query.Option) Option {
+	return func(builder *Builder) {
+		templatedVar := query.New(name, options...)
+
+		builder.board.Templating.List = append(builder.board.Templating.List, templatedVar.Builder)
+	}
+}
+
+// Row adds a row to the dashboard.
+func Row(title string, options ...row.Option) Option {
+	return func(builder *Builder) {
+		row.New(builder.board, title, options...)
+	}
+}
+
+// TagsAnnotation adds a new source of annotation for the dashboard.
+func TagsAnnotation(annotation TagAnnotation) Option {
+	return func(builder *Builder) {
+		builder.board.Annotations.List = append(builder.board.Annotations.List, sdk.Annotation{
+			Name:       annotation.Name,
+			Datasource: &annotation.Datasource,
+			IconColor:  annotation.IconColor,
+			Enable:     true,
+			Tags:       annotation.Tags,
+			Type:       "tags",
+		})
+	}
+}
+
+// Editable marks the dashboard as editable.
+func Editable() Option {
+	return func(builder *Builder) {
+		builder.board.Editable = true
+	}
+}
+
+// ReadOnly marks the dashboard as non-editable.
+func ReadOnly() Option {
+	return func(builder *Builder) {
+		builder.board.Editable = false
+	}
+}
+
+// SharedCrossHair configures the graph tooltip to be shared across panels.
+func SharedCrossHair() Option {
+	return func(builder *Builder) {
+		builder.board.SharedCrosshair = true
+	}
+}
+
+// DefaultTooltip configures the graph tooltip NOT to be shared across panels.
+func DefaultTooltip() Option {
+	return func(builder *Builder) {
+		builder.board.SharedCrosshair = false
+	}
+}
+
+// Tags adds the given set of tags to the dashboard.
+func Tags(tags []string) Option {
+	return func(builder *Builder) {
+		builder.board.Tags = tags
+	}
+}
+
+// AutoRefresh defines the auto-refresh interval for the dashboard.
+func AutoRefresh(interval string) Option {
+	return func(builder *Builder) {
+		builder.board.Refresh = &sdk.BoolString{Flag: true, Value: interval}
+	}
+}
