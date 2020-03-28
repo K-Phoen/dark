008fe4025e4c0aa39dd64754b5137e4d7bb03b4b
Kévin Gomez <kevin.gomez@voiapp.io>
2020-03-28 19:51:52 +0100

    Update codegen script

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -8,13 +8,13 @@ SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
-  github.com/K-Phoen/dark/pkg/generated github.com/K-Phoen/dark/pkg/apis \
+  github.com/K-Phoen/dark/internal/pkg/generated github.com/K-Phoen/dark/internal/pkg/apis \
   controller:v1 \
   --output-base "$(dirname "${BASH_SOURCE[0]}")/.." \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
 
-mv github.com/K-Phoen/dark/pkg/apis/controller/v1/zz_generated.deepcopy.go pkg/apis/controller/v1/zz_generated.deepcopy.go
+mv github.com/K-Phoen/dark/internal/pkg/apis/controller/v1/zz_generated.deepcopy.go internal/pkg/apis/controller/v1/zz_generated.deepcopy.go
 
-rm -rf pkg/generated
-mv github.com/K-Phoen/dark/pkg/generated pkg/generated
+rm -rf internal/pkg/generated
+mv github.com/K-Phoen/dark/internal/pkg/generated internal/pkg
 rm -rf github.com/
