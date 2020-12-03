a82972c74aa797e634da34ac9cdceefbf32cefb4
Kévin Gomez <kevin.gomez@voiapp.io>
2020-12-03 18:24:10 +0100

    Support more grabana features

--- internal/pkg/converter/json.go
+++ internal/pkg/converter/json.go
@@ -161,6 +161,8 @@ func (converter *JSON) convertVariable(variable sdk.TemplateVar, dashboard *grab
 		converter.convertQueryVar(variable, dashboard)
 	case "const":
 		converter.convertConstVar(variable, dashboard)
+	case "datasource":
+		converter.convertDatasourceVar(variable, dashboard)
 	default:
 		converter.logger.Warn("unhandled variable type found: skipped", zap.String("type", variable.Type), zap.String("name", variable.Name))
 	}
@@ -207,11 +209,25 @@ func (converter *JSON) convertQueryVar(variable sdk.TemplateVar, dashboard *grab
 		Label:      variable.Label,
 		Datasource: datasource,
 		Request:    variable.Query,
+		Regex:      variable.Regex,
 		IncludeAll: variable.IncludeAll,
 		DefaultAll: variable.Current.Value == "$__all",
+		AllValue:   variable.AllValue,
 	}
 
 	dashboard.Variables = append(dashboard.Variables, grabana.DashboardVariable{Query: query})
+}
+
+func (converter *JSON) convertDatasourceVar(variable sdk.TemplateVar, dashboard *grabana.DashboardModel) {
+	datasource := &grabana.VariableDatasource{
+		Name:       variable.Name,
+		Label:      variable.Label,
+		Type:       variable.Query,
+		Regex:      variable.Regex,
+		IncludeAll: variable.IncludeAll,
+	}
+
+	dashboard.Variables = append(dashboard.Variables, grabana.DashboardVariable{Datasource: datasource})
 }
 
 func (converter *JSON) convertConstVar(variable sdk.TemplateVar, dashboard *grabana.DashboardModel) {
@@ -272,6 +288,8 @@ func (converter *JSON) convertDataPanel(panel sdk.Panel) (grabana.DashboardPanel
 		return converter.convertSingleStat(panel), true
 	case "table":
 		return converter.convertTable(panel), true
+	case "text":
+		return converter.convertText(panel), true
 	default:
 		converter.logger.Warn("unhandled panel type: skipped", zap.String("type", panel.Type), zap.String("title", panel.Title))
 	}
@@ -288,8 +306,9 @@ func (converter *JSON) convertRow(panel sdk.Panel) *grabana.DashboardRow {
 
 func (converter *JSON) convertGraph(panel sdk.Panel) grabana.DashboardPanel {
 	graph := &grabana.DashboardGraph{
-		Title: panel.Title,
-		Span:  panelSpan(panel),
+		Title:       panel.Title,
+		Span:        panelSpan(panel),
+		Transparent: panel.Transparent,
 		Axes: &grabana.GraphAxes{
 			Bottom: converter.convertAxis(panel.Xaxis),
 		},
@@ -381,10 +400,11 @@ func (converter *JSON) convertAxis(sdkAxis sdk.Axis) *grabana.GraphAxis {
 
 func (converter *JSON) convertSingleStat(panel sdk.Panel) grabana.DashboardPanel {
 	singleStat := &grabana.DashboardSingleStat{
-		Title:     panel.Title,
-		Span:      panelSpan(panel),
-		Unit:      panel.SinglestatPanel.Format,
-		ValueType: panel.SinglestatPanel.ValueName,
+		Title:       panel.Title,
+		Span:        panelSpan(panel),
+		Unit:        panel.SinglestatPanel.Format,
+		ValueType:   panel.SinglestatPanel.ValueName,
+		Transparent: panel.Transparent,
 	}
 
 	if panel.Height != nil {
@@ -439,8 +459,9 @@ func (converter *JSON) convertSingleStat(panel sdk.Panel) grabana.DashboardPanel
 
 func (converter *JSON) convertTable(panel sdk.Panel) grabana.DashboardPanel {
 	table := &grabana.DashboardTable{
-		Title: panel.Title,
-		Span:  panelSpan(panel),
+		Title:       panel.Title,
+		Span:        panelSpan(panel),
+		Transparent: panel.Transparent,
 	}
 
 	if panel.Height != nil {
@@ -481,6 +502,26 @@ func (converter *JSON) convertTable(panel sdk.Panel) grabana.DashboardPanel {
 	}
 
 	return grabana.DashboardPanel{Table: table}
+}
+
+func (converter *JSON) convertText(panel sdk.Panel) grabana.DashboardPanel {
+	text := &grabana.DashboardText{
+		Title:       panel.Title,
+		Span:        panelSpan(panel),
+		Transparent: panel.Transparent,
+	}
+
+	if panel.Height != nil {
+		text.Height = *panel.Height
+	}
+
+	if panel.TextPanel.Mode == "markdown" {
+		text.Markdown = panel.TextPanel.Content
+	} else {
+		text.HTML = panel.TextPanel.Content
+	}
+
+	return grabana.DashboardPanel{Text: text}
 }
 
 func (converter *JSON) convertTarget(target sdk.Target) *grabana.Target {
