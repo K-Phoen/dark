8684bc0429f03a43ee52804a8b8c0b808135af3e
Kévin Gomez <kevin.gomez@voiapp.io>
2021-03-27 18:20:56 +0100

    Support alert conversion from json to yaml

--- internal/pkg/converter/json.go
+++ internal/pkg/converter/json.go
@@ -318,6 +318,7 @@ func (converter *JSON) convertGraph(panel sdk.Panel) grabana.DashboardPanel {
 		},
 		Legend:        converter.convertLegend(panel.GraphPanel.Legend),
 		Visualization: converter.convertVisualization(panel),
+		Alert:         converter.convertAlert(panel),
 	}
 
 	if panel.Height != nil {
@@ -342,6 +343,71 @@ func (converter *JSON) convertGraph(panel sdk.Panel) grabana.DashboardPanel {
 	}
 
 	return grabana.DashboardPanel{Graph: graph}
+}
+
+func (converter *JSON) convertAlert(panel sdk.Panel) *grabana.GraphAlert {
+	if panel.Alert == nil {
+		return nil
+	}
+
+	sdkAlert := panel.Alert
+
+	notifications := make([]string, 0, len(sdkAlert.Notifications))
+	for _, notification := range sdkAlert.Notifications {
+		notifications = append(notifications, notification.UID)
+	}
+
+	alert := &grabana.GraphAlert{
+		Title:            sdkAlert.Name,
+		Message:          sdkAlert.Message,
+		EvaluateEvery:    sdkAlert.Frequency,
+		For:              sdkAlert.For,
+		Tags:             sdkAlert.AlertRuleTags,
+		OnNoData:         sdkAlert.NoDataState,
+		OnExecutionError: sdkAlert.ExecutionErrorState,
+		Notifications:    notifications,
+		If:               converter.convertAlertConditions(sdkAlert),
+	}
+
+	return alert
+}
+
+func (converter *JSON) convertAlertConditions(sdkAlert *sdk.Alert) []grabana.AlertCondition {
+	conditions := make([]grabana.AlertCondition, 0, len(sdkAlert.Conditions))
+
+	for _, condition := range sdkAlert.Conditions {
+		conditions = append(conditions, grabana.AlertCondition{
+			Operand: condition.Operator.Type,
+			Value: grabana.AlertValue{
+				Func:     condition.Reducer.Type,
+				QueryRef: condition.Query.Params[0],
+				From:     condition.Query.Params[1],
+				To:       condition.Query.Params[2],
+			},
+			Threshold: converter.convertAlertThreshold(condition),
+		})
+	}
+
+	return conditions
+}
+
+func (converter *JSON) convertAlertThreshold(sdkCondition sdk.AlertCondition) grabana.AlertThreshold {
+	threshold := grabana.AlertThreshold{}
+
+	switch sdkCondition.Evaluator.Type {
+	case "no_value":
+		threshold.HasNoValue = true
+	case "lt":
+		threshold.Below = &sdkCondition.Evaluator.Params[0]
+	case "gt":
+		threshold.Above = &sdkCondition.Evaluator.Params[0]
+	case "outside_range":
+		threshold.OutsideRange = [2]float64{sdkCondition.Evaluator.Params[0], sdkCondition.Evaluator.Params[1]}
+	case "within_range":
+		threshold.WithinRange = [2]float64{sdkCondition.Evaluator.Params[0], sdkCondition.Evaluator.Params[1]}
+	}
+
+	return threshold
 }
 
 func (converter *JSON) convertVisualization(panel sdk.Panel) *grabana.GraphVisualization {
