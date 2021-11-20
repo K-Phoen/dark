1134fdecb21e6c44807d66b4d3f0e678166847d2
Kévin Gomez <contact@kevingomez.fr>
2021-11-20 22:41:00 +0100

    Split json decoder in several files

---
+++ internal/pkg/converter/graph_test.go
@@ -0,0 +1,182 @@
+package converter
+
+import (
+	"testing"
+
+	"github.com/K-Phoen/sdk"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+func TestConvertGraphPanel(t *testing.T) {
+	req := require.New(t)
+
+	converter := NewJSON(zap.NewNop())
+	height := "400px"
+	datasource := "prometheus"
+
+	graphPanel := sdk.Panel{
+		CommonPanel: sdk.CommonPanel{
+			Title:       "graph panel",
+			Type:        "graph",
+			Description: strPtr("graph description"),
+			Transparent: true,
+			Height:      &height,
+			Datasource:  &datasource,
+		},
+		GraphPanel: &sdk.GraphPanel{},
+	}
+
+	converted, ok := converter.convertDataPanel(graphPanel)
+
+	req.True(ok)
+	req.NotNil(converted.Graph)
+
+	graph := converted.Graph
+	req.True(graph.Transparent)
+	req.Equal("graph panel", graph.Title)
+	req.Equal("graph description", graph.Description)
+	req.Equal(height, graph.Height)
+	req.Equal(datasource, graph.Datasource)
+}
+
+func TestConvertGraphLegend(t *testing.T) {
+	req := require.New(t)
+
+	converter := NewJSON(zap.NewNop())
+
+	rawLegend := sdk.Legend{
+		AlignAsTable: true,
+		Avg:          true,
+		Current:      true,
+		HideEmpty:    true,
+		HideZero:     true,
+		Max:          true,
+		Min:          true,
+		RightSide:    true,
+		Show:         true,
+		Total:        true,
+	}
+
+	legend := converter.convertGraphLegend(rawLegend)
+
+	req.ElementsMatch(
+		[]string{"as_table", "to_the_right", "min", "max", "avg", "current", "total", "no_null_series", "no_zero_series"},
+		legend,
+	)
+}
+
+func TestConvertGraphCanHideLegend(t *testing.T) {
+	req := require.New(t)
+	converter := NewJSON(zap.NewNop())
+
+	legend := converter.convertGraphLegend(sdk.Legend{Show: false})
+	req.ElementsMatch([]string{"hide"}, legend)
+}
+
+func TestConvertGraphAxis(t *testing.T) {
+	req := require.New(t)
+
+	converter := NewJSON(zap.NewNop())
+
+	rawAxis := sdk.Axis{
+		Format:  "bytes",
+		LogBase: 2,
+		Min:     &sdk.FloatString{Value: 0},
+		Max:     &sdk.FloatString{Value: 42},
+		Show:    true,
+		Label:   "Axis",
+	}
+
+	axis := converter.convertGraphAxis(rawAxis)
+
+	req.Equal("bytes", *axis.Unit)
+	req.Equal("Axis", axis.Label)
+	req.EqualValues(0, *axis.Min)
+	req.EqualValues(42, *axis.Max)
+	req.False(*axis.Hidden)
+}
+
+func TestConvertGraphVisualization(t *testing.T) {
+	req := require.New(t)
+	converter := NewJSON(zap.NewNop())
+	enabled := true
+
+	graphPanel := sdk.Panel{
+		CommonPanel: sdk.CommonPanel{
+			Title: "graph panel",
+			Type:  "graph",
+		},
+		GraphPanel: &sdk.GraphPanel{
+			NullPointMode: "connected",
+			SteppedLine:   true,
+			SeriesOverrides: []sdk.SeriesOverride{
+				{
+					Alias:  "alias",
+					Dashes: &enabled,
+				},
+			},
+		},
+	}
+
+	visualization := converter.convertGraphVisualization(graphPanel)
+
+	req.True(visualization.Staircase)
+	req.Equal("connected", visualization.NullValue)
+	req.Len(visualization.Overrides, 1)
+}
+
+func TestConvertGraphOverridesWithNoOverride(t *testing.T) {
+	req := require.New(t)
+	converter := NewJSON(zap.NewNop())
+
+	graphPanel := sdk.Panel{
+		CommonPanel: sdk.CommonPanel{
+			Title: "graph panel",
+			Type:  "graph",
+		},
+		GraphPanel: &sdk.GraphPanel{},
+	}
+
+	overrides := converter.convertGraphOverrides(graphPanel)
+
+	req.Len(overrides, 0)
+}
+
+func TestConvertGraphOverridesWithOneOverride(t *testing.T) {
+	req := require.New(t)
+	converter := NewJSON(zap.NewNop())
+	color := "red"
+	enabled := true
+	number := 2
+
+	graphPanel := sdk.Panel{
+		CommonPanel: sdk.CommonPanel{
+			Title: "heatmap panel",
+			Type:  "graph",
+		},
+		GraphPanel: &sdk.GraphPanel{
+			SeriesOverrides: []sdk.SeriesOverride{
+				{
+					Alias:  "alias",
+					Color:  &color,
+					Dashes: &enabled,
+					Fill:   &number,
+					Lines:  &enabled,
+				},
+			},
+		},
+	}
+
+	overrides := converter.convertGraphOverrides(graphPanel)
+
+	req.Len(overrides, 1)
+
+	override := overrides[0]
+
+	req.Equal("alias", override.Alias)
+	req.Equal(color, override.Color)
+	req.True(*override.Dashes)
+	req.True(*override.Lines)
+	req.Equal(number, *override.Fill)
+}
---
+++ internal/pkg/converter/single_stat.go
@@ -0,0 +1,116 @@
+package converter
+
+import (
+	"strings"
+
+	grabana "github.com/K-Phoen/grabana/decoder"
+	"github.com/K-Phoen/grabana/singlestat"
+	"github.com/K-Phoen/sdk"
+)
+
+func (converter *JSON) convertSingleStat(panel sdk.Panel) grabana.DashboardPanel {
+	singleStat := &grabana.DashboardSingleStat{
+		Title:         panel.Title,
+		Span:          panelSpan(panel),
+		Unit:          panel.SinglestatPanel.Format,
+		Decimals:      &panel.SinglestatPanel.Decimals,
+		ValueType:     panel.SinglestatPanel.ValueName,
+		Transparent:   panel.Transparent,
+		ValueFontSize: panel.SinglestatPanel.ValueFontSize,
+	}
+
+	if panel.Description != nil {
+		singleStat.Description = *panel.Description
+	}
+	if panel.Repeat != nil {
+		singleStat.Repeat = *panel.Repeat
+	}
+	if panel.Height != nil {
+		singleStat.Height = *(panel.Height).(*string)
+	}
+	if panel.Datasource != nil {
+		singleStat.Datasource = *panel.Datasource
+	}
+
+	thresholds := strings.Split(panel.SinglestatPanel.Thresholds, ",")
+	if len(thresholds) == 2 {
+		singleStat.Thresholds = [2]string{thresholds[0], thresholds[1]}
+	}
+
+	if len(panel.SinglestatPanel.Colors) == 3 {
+		singleStat.Colors = [3]string{
+			panel.SinglestatPanel.Colors[0],
+			panel.SinglestatPanel.Colors[1],
+			panel.SinglestatPanel.Colors[2],
+		}
+	}
+
+	var colorOpts []string
+	if panel.SinglestatPanel.ColorBackground {
+		colorOpts = append(colorOpts, "background")
+	}
+	if panel.SinglestatPanel.ColorValue {
+		colorOpts = append(colorOpts, "value")
+	}
+	if len(colorOpts) != 0 {
+		singleStat.Color = colorOpts
+	}
+
+	if panel.SinglestatPanel.SparkLine.Show && panel.SinglestatPanel.SparkLine.Full {
+		singleStat.SparkLine = "full"
+	}
+	if panel.SinglestatPanel.SparkLine.Show && !panel.SinglestatPanel.SparkLine.Full {
+		singleStat.SparkLine = "bottom"
+	}
+
+	// Font sizes
+	if panel.SinglestatPanel.PrefixFontSize != nil && *panel.SinglestatPanel.PrefixFontSize != "" {
+		singleStat.PrefixFontSize = *panel.SinglestatPanel.PrefixFontSize
+	}
+	if panel.SinglestatPanel.PostfixFontSize != nil && *panel.SinglestatPanel.PostfixFontSize != "" {
+		singleStat.PostfixFontSize = *panel.SinglestatPanel.PostfixFontSize
+	}
+
+	// ranges to text mapping
+	singleStat.RangesToText = converter.convertSingleStatRangesToText(panel)
+
+	for _, target := range panel.SinglestatPanel.Targets {
+		graphTarget := converter.convertTarget(target)
+		if graphTarget == nil {
+			continue
+		}
+
+		singleStat.Targets = append(singleStat.Targets, *graphTarget)
+	}
+
+	return grabana.DashboardPanel{SingleStat: singleStat}
+}
+
+func (converter *JSON) convertSingleStatRangesToText(panel sdk.Panel) []singlestat.RangeMap {
+	if panel.SinglestatPanel.MappingType == nil || *panel.SinglestatPanel.MappingType != 2 {
+		return nil
+	}
+
+	mappings := make([]singlestat.RangeMap, 0, len(panel.SinglestatPanel.RangeMaps))
+	for _, mapping := range panel.SinglestatPanel.RangeMaps {
+		converted := singlestat.RangeMap{
+			From: "",
+			To:   "",
+			Text: "",
+		}
+
+		if mapping.From != nil {
+			converted.From = *mapping.From
+		}
+		if mapping.To != nil {
+			converted.To = *mapping.To
+		}
+		if mapping.Text != nil {
+			converted.Text = *mapping.Text
+		}
+
+		mappings = append(mappings, converted)
+	}
+
+	return mappings
+}
